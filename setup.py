6b6707e9b60e2e877ecc1daad6a5054a1e36367e
Camilo Galindo <jucamilogal7@gmail.com>
2025-06-18 19:20:17 -0500

    primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tercerRepo",                              # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Camilo Galindo",                         # Tu nombre
+    author_email="jucamilogal7@gmail.com",                 # Tu correo electrónico
+    url="https://github.com/jucamilogal/tercerRepo",     # URL del proyecto
+)
